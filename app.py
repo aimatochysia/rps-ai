3a7bc10781012877bd3e151e44c50537970bf51d
Petra Michael <petraxmichael@gmail.com>
2025-11-25 13:24:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,23 +4,24 @@ import numpy as np
 from PIL import Image
 from fastapi.middleware.cors import CORSMiddleware
 
-
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 session = ort.InferenceSession("rps_ai.onnx")
+input_name = session.get_inputs()[0].name
 
 @app.post("/detect")
 async def detect(file: UploadFile = File(...)):
     img = Image.open(file.file).convert("RGB")
-    img = np.array(img).transpose(2,0,1) / 255.0
-    img = img[None].astype(np.float32)
-    outputs = session.run(None, {"input": img})
+    img = np.array(img).transpose(2,0,1).astype(np.float32) / 255.0
+    img = np.expand_dims(img, 0)
+
+    outputs = session.run(None, {input_name: img})
     return {"detections": outputs[0].tolist()}
